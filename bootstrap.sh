bd957554fd8fb006c50e240b4fb4b6d7a0b87cd9
meoflynn <megan.o'flynn@stfc.ac.uk>
2024-11-06 15:07:50 +0000

    ENH: Update variable name

--- bootstrap.sh
+++ bootstrap.sh
@@ -90,7 +90,7 @@ echo "Importing required helm repos and packages"
 helm repo add capi https://azimuth-cloud.github.io/capi-helm-charts
 helm repo add capi-addons https://azimuth-cloud.github.io/cluster-api-addon-provider
 helm repo update
-helm upgrade cluster-api-addon-provider capi-addons/cluster-api-addon-provider --create-namespace --install --wait -n clusters --version "${CAPO_ADDON_VERSION}"
+helm upgrade cluster-api-addon-provider capi-addons/cluster-api-addon-provider --create-namespace --install --wait -n clusters --version "${AZIMUTH_CAPO_ADDON_VERSION}"
 
 
 echo "You are now ready to create a cluster following the remaining instructions..."

0cb188870d0da6235fb12b3506ae346ef764e294
AnishMudaraddi <anish.mudaraddi@stfc.ac.uk>
2026-02-05 14:05:42 +0000

    use apt to install yq instead of snap
    add check if yq is already installed for yq so we're not forced to initiate apt if it is already installed

--- set-env.sh
+++ set-env.sh
@@ -4,9 +4,10 @@
 # if we do this and run into any errors it will kill the user's
 # current shell (e.g. a typo)
 
-echo "Installing required tools..."
-export PATH=$PATH:/snap/bin
-sudo snap install yq
+if [ ! -f "/usr/bin/yq" ]; then
+    echo "Installing yq..."
+    sudo apt-get -y install yq
+fi
 
 # Function to convert dependencies to a valid environment variables
 sanitize_var_name() {
